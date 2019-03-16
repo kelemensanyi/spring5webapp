36082442fa87bebd5894b1e06c63f020ad5fb1b5
Sandor Kelemen <kelemen.sandor@efox.hu>
2019-03-16 22:59:58 +0100

    New associations not preserved

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring5webapp;
 
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,21 @@ public class Spring5webappApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void newAssociatonsPreserved() {
+		Book book = new Book("Title", "isbn", "publisher");
+		Author firstAuthor = new Author("First", "Last");
+		Author otherAuthor = new Author("OtherFirst", "OtherLast");
+
+		book.getAuthors().add(firstAuthor);
+		firstAuthor.getBooks().add(book);
+
+		book.getAuthors().add(otherAuthor);
+		otherAuthor.getBooks().add(book);
+
+		assert book.getAuthors().size() == 2;
 	}
 
 }
